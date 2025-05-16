c330fa56b652c2f86aa99365e73004f57dce9c6f
nicoriera <nicolariera@gmail.com>
2025-05-16 12:05:06 +0200

    chore: update @nicoriera/react-modal-converted to version 1.0.9 and enhance Modal component styling

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -12,8 +12,16 @@ interface ModalProps {
 const Modal = ({ isOpen, onClose, title, children }: ModalProps) => {
   console.log("Modal rendered, isOpen:", isOpen);
   return (
-    <ReactModalConverted isOpen={isOpen} onClose={onClose}>
-      {title && <h2>{title}</h2>}
+    <ReactModalConverted
+      isOpen={isOpen}
+      onClose={onClose}
+      overlayClassName="fixed inset-0 z-50 flex items-center justify-center bg-black/50  transition-opacity duration-300 ease-in-out"
+      modalClassName="relative bg-white rounded-lg shadow-xl p-6 m-4 max-w-sm w-full transform transition-all duration-300 ease-in-out scale-95  animate-fade-in-scale">
+      {title && (
+        <h2 className="text-xl font-bold mb-4" id="modal-title">
+          {title}
+        </h2>
+      )}
       {children}
     </ReactModalConverted>
   );
